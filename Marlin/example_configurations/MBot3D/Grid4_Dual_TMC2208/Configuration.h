51180f42e7591a3a1cc937d1bdc4b18eca1d7052
JIN Tao <jeffkyjin@gmail.com>
2019-05-06 13:04:44 +0800

    change auto bed leveling first point x to 100 y to 228

--- Marlin/example_configurations/MBot3D/Grid4_Dual_TMC2208/Configuration.h
+++ Marlin/example_configurations/MBot3D/Grid4_Dual_TMC2208/Configuration.h
@@ -1080,7 +1080,7 @@
  */
  #if ENABLED(AUTO_BED_LEVELING_3POINT) || ENABLED(AUTO_BED_LEVELING_UBL)
   #define PROBE_PT_1_X 100
-  #define PROBE_PT_1_Y 224
+  #define PROBE_PT_1_Y 228
   #define PROBE_PT_2_X 210
   #define PROBE_PT_2_Y 0
   #define PROBE_PT_3_X 210
