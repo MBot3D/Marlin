1c6e9cada1c291d5bd024c0024c031159df27557
JIN Tao <jeffkyjin@gmail.com>
2019-07-12 14:00:40 +0800

    update auto leveling 3 point , z max feed rate to 2

--- Marlin/example_configurations/MBot3D/M5000_TMC2208/Configuration.h
+++ Marlin/example_configurations/MBot3D/M5000_TMC2208/Configuration.h
@@ -607,7 +607,7 @@
  * Override with M203
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4]]]]
  */
-#define DEFAULT_MAX_FEEDRATE          { 300, 300, 3.3, 25}  //{ 300, 300, 5, 25}
+#define DEFAULT_MAX_FEEDRATE          { 300, 300, 2, 25}  //{ 300, 300, 5, 25}
 
 /**
  * Default Max Acceleration (change/s) change = mm/s
@@ -637,10 +637,10 @@
  * When changing speed and direction, if the difference is less than the
  * value set here, it may happen instantaneously.
  */
-#define DEFAULT_XJERK                 10.0//20.0
-#define DEFAULT_YJERK                 10.0//20.0
-#define DEFAULT_ZJERK                  0.2//0.4
-#define DEFAULT_EJERK                  2.5//5.0
+#define DEFAULT_XJERK                 20.0
+#define DEFAULT_YJERK                 20.0
+#define DEFAULT_ZJERK                 0.4
+#define DEFAULT_EJERK                 5.0
 
 /**
  * S-Curve Acceleration
