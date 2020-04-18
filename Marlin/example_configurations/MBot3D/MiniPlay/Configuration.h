ab6ea167b8faf6893b7e489c48abc108f96b8cfb
JIN Tao <jeffkyjin@gmail.com>
2020-04-18 20:13:36 +0800

    Update Configuration.h
    change size to 120

--- Marlin/example_configurations/MBot3D/MiniPlay/Configuration.h
+++ Marlin/example_configurations/MBot3D/MiniPlay/Configuration.h
@@ -871,8 +871,8 @@
 // @section machine
 
 // The size of the print bed
-#define X_BED_SIZE 150
-#define Y_BED_SIZE 150
+#define X_BED_SIZE 120
+#define Y_BED_SIZE 120
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
