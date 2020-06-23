b0d4ec8b0b6886d2ee5ad3ae962b4a87c7d935b2
JIN Tao <jeffkyjin@gmail.com>
2020-06-23 17:32:16 +0800

    Update Configuration.h

--- Marlin/example_configurations/MBot3D/MiniPlay/Configuration.h
+++ Marlin/example_configurations/MBot3D/MiniPlay/Configuration.h
@@ -871,8 +871,8 @@
 // @section machine
 
 // The size of the print bed
-#define X_BED_SIZE 130
-#define Y_BED_SIZE 130
+#define X_BED_SIZE 140
+#define Y_BED_SIZE 140
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
@@ -880,7 +880,7 @@
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
 #define Y_MAX_POS Y_BED_SIZE
-#define Z_MAX_POS 130
+#define Z_MAX_POS 140
 
 /**
  * Software Endstops
